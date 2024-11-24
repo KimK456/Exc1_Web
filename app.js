a3988838c0490cb2c2041c81e2b67b5c67684928
Kim Kurikan <kim.kurikan@gmail.com>
2024-11-24 11:30:17 +0200

    Added DB connection

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', error=>{console.error.bind(console, 'connection error:')})
+db.once('open', ()=>console.log('connected to mongo'))
+
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const posts_routes = require("./routes/posts_routes")
+
 app.listen(port, () => {
     console.log(`listening on http://loacalhost:${port}`)
 });
--- routes/posts_routes.js
+++ routes/posts_routes.js
@@ -1,5 +1,5 @@
 const express = require("express")
 const router = express.Router()
-const Post = require("../controllers/post")
+const Post = require("../controllers/posts_controller")
 
 module.exports = router

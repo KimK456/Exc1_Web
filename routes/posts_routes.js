37f18c46d921e9083ea28ea6cb1caa3d2b4c9b70
Kim Kurikan <kim.kurikan@gmail.com>
2024-11-24 20:44:54 +0200

    added createPost API

--- routes/posts_routes.js
+++ routes/posts_routes.js
@@ -2,4 +2,6 @@ const express = require("express")
 const router = express.Router()
 const Post = require("../controllers/posts_controller")
 
+router.post("/", Post.createPost)
+
 module.exports = router

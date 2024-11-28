08ef11a67c33c25f3828a5475dacac9ce530049f
Kim Kurikan <kim.kurikan@gmail.com>
2024-11-28 16:33:42 +0200

    added get-all-posts api

--- routes/posts_routes.js
+++ routes/posts_routes.js
@@ -2,6 +2,8 @@ const express = require("express")
 const router = express.Router()
 const Post = require("../controllers/posts_controller")
 
+router.get("/", Post.getAllPosts)
+
 router.post("/", Post.createPost)
 
 module.exports = router

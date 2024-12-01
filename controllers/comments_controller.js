c91bb69c959e5d60659e072d5d9ce772a46b84f1
Kim Kurikan <kim.kurikan@gmail.com>
2024-12-01 18:38:18 +0200

    added get all comments api

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,15 @@
 const Comments = require("../models/comments_model")
 const Posts = require("../models/posts_model")
 
+const getAllComments = async (req, res) => {
+    try {
+        const comments = await Comments.find();
+        res.send(comments);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
 const createComment = async (req, res) => {
     const postId = req.body.postId
     try {
@@ -18,5 +27,6 @@ const createComment = async (req, res) => {
 }
 
 module.exports = {
+    getAllComments,
     createComment
 }
